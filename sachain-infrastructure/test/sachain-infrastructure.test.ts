6458a6759eea59425f8758dbab719dea44b63125
Sandjio <emmasandjio@gmail.com>
2025-08-11 13:16:34 +0100

    chore(sachain-infrastructure): initialize new AWS CDK app with TypeScript

---
+++ sachain-infrastructure/test/sachain-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SachainInfrastructure from '../lib/sachain-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sachain-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SachainInfrastructure.SachainInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
