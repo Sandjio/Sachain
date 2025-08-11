d4976aa614214afebf739897f6e02ca8d37e4aa5
Sandjio <emmasandjio@gmail.com>
2025-08-11 16:25:50 +0100

    feat: add coverage support

--- sachain-infrastructure/jest.config.js
+++ sachain-infrastructure/jest.config.js
@@ -10,4 +10,6 @@ module.exports = {
       },
     ],
   },
+  collectCoverage: true,
+  coverageDirectory: "coverage",
 };
---
+++ sachain-infrastructure/test/setup.ts
@@ -0,0 +1,2 @@
+// Jest setup file for CDK infrastructure tests
+// This file is used to configure Jest for CDK testing
