6420e0488f101579d06671708481e643d68a9c3d
Sandjio <emmasandjio@gmail.com>
2025-09-11 17:53:00 +0100

    feat: remove conditions on the Project Creation Role

--- sachain-infrastructure/lib/constructs/security.ts
+++ sachain-infrastructure/lib/constructs/security.ts
@@ -503,16 +503,6 @@ export class SecurityConstruct extends Construct {
           "dynamodb:Scan",
         ],
         resources: [this.table.tableArn, `${this.table.tableArn}/index/*`],
-        conditions: {
-          "ForAllValues:StringLike": {
-            "dynamodb:LeadingKeys": [
-              "USER#*",
-              "PROJECT#*",
-              "AUDIT#*",
-              "COMPLIANCE#*",
-            ],
-          },
-        },
       })
     );
 
